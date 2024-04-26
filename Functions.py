d075e2058b183db2beb8f9374fbf0a91674377ca
SigneDjernis <signedjernis@gmail.com>
2024-04-26 16:49:03 +0200

    Vectorization

--- Functions.py
+++ Functions.py
@@ -29,6 +29,26 @@ def Loss_function_prior(point,Y,k,dis,alpha):
     return result
 
 
+def Loss_function_prior_fast(point,Y,k,alpha):
+    Y = np.array(Y)
+    connections = Y[:, 0]
+    indices_a = Y[:, 1]
+    indices_b = Y[:, 2]
+
+    points_a = [point[index] for index in indices_a]
+    points_b = [point[index] for index in indices_b]
+    points_a_np = np.array(points_a)
+    points_b_np = np.array(points_b)
+
+    distances = np.sum((points_a_np - points_b_np) ** 2, axis=1)
+
+    sigmoid_value = 1 / (1 + np.exp(-connections * (alpha - distances)))
+    prior = np.log(1/(2*math.pi)**(k/2)) - 1/2 * np.sum(np.square(list(point.values()))**2)
+    return np.sum(np.log(sigmoid_value)) + prior
+
+
+
+
 # Gradient function with prior
 ## The Gradient_function_prior take as input the point number, the index, Y-matrix, all the points, the distance metric, alpha and reuturns the gradient for the point number's index
 ## It loops over all possible edges (every row in Y), and checks if point_number is in that row, if so it calculates the gradient
@@ -56,6 +76,30 @@ def Gradient_function_prior(point_number,index,Y,point,dis,alpha):
     return gradient 
 
 
+def Gradient_function_prior_fast(point_number,index,Y,point,alpha):
+    Y = np.array(Y)
+    mask_a = (Y[:, 1] == point_number)
+    mask_b = (Y[:, 2] == point_number)
+
+    Y_edges = Y[mask_a+mask_b]
+    connections = Y_edges[:, 0]
+    indices_a = Y_edges[:, 1]
+    indices_b = Y_edges[:, 2]
+
+    points_a = [point[index] for index in indices_a]
+    points_b = [point[index] for index in indices_b]
+    points_a_np = np.array(points_a)
+    points_b_np = np.array(points_b)
+
+    distances = np.sum((points_a_np - points_b_np) ** 2, axis=1)
+
+    mask = mask_a + mask_b
+    point_index_diff = (points_a_np[:,index] - points_b_np[:,index]) * mask_a[mask] + (points_b_np[:,index] - points_a_np[:,index]) * mask_b[mask]
+
+    numerators = -2 * connections * point_index_diff * np.exp(-connections * (alpha - distances)) 
+    denominators = 1 + np.exp(-connections * (alpha - distances)) 
+
+    return np.sum(numerators / denominators) - point[point_number][index]
 
 
 ########################################################### Without Prior ###########################################################
@@ -78,6 +122,23 @@ def Loss_function(point,Y,dis,alpha):
         result += np.log(sigmoid_value) 
 
     return result
+
+
+def Loss_function_fast(point,Y,alpha):
+    Y = np.array(Y)
+    connections = Y[:, 0]
+    indices_a = Y[:, 1]
+    indices_b = Y[:, 2]
+
+    points_a = [point[index] for index in indices_a]
+    points_b = [point[index] for index in indices_b]
+    points_a_np = np.array(points_a)
+    points_b_np = np.array(points_b)
+
+    distances = np.sum((points_a_np - points_b_np) ** 2, axis=1)
+
+    sigmoid_value = 1 / (1 + np.exp(-connections * (alpha - distances)))
+    return np.sum(np.log(sigmoid_value))
 
 
 # Gradient function
@@ -103,6 +164,32 @@ def Gradient_function(point_number,index,Y,point,dis,alpha):
             gradient += numerator / denominator 
 
     return gradient 
+
+
+def Gradient_function_fast(point_number,index,Y,point,alpha):
+    Y = np.array(Y)
+    mask_a = (Y[:, 1] == point_number)
+    mask_b = (Y[:, 2] == point_number)
+
+    Y_edges = Y[mask_a+mask_b]
+    connections = Y_edges[:, 0]
+    indices_a = Y_edges[:, 1]
+    indices_b = Y_edges[:, 2]
+
+    points_a = [point[index] for index in indices_a]
+    points_b = [point[index] for index in indices_b]
+    points_a_np = np.array(points_a)
+    points_b_np = np.array(points_b)
+
+    distances = np.sum((points_a_np - points_b_np) ** 2, axis=1)
+
+    mask = mask_a + mask_b
+    point_index_diff = (points_a_np[:,index] - points_b_np[:,index]) * mask_a[mask] + (points_b_np[:,index] - points_a_np[:,index]) * mask_b[mask]
+
+    numerators = -2 * connections * point_index_diff * np.exp(-connections * (alpha - distances)) 
+    denominators = 1 + np.exp(-connections * (alpha - distances)) 
+
+    return np.sum(numerators / denominators)
 
 
 
